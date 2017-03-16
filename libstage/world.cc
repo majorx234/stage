b4925ee224758635febfde77c5c752c7bf06fa0e
major major <majorx234@gmail.com>
2017-03-16 01:01:28 +0100

    fixed abs function input, cast double to int32 bevor calculate absolute values

--- libstage/world.cc
+++ libstage/world.cc
@@ -821,8 +821,8 @@ RaytraceResult World::Raytrace( const Ray& r )
   // Graphics Gems IV  
   const int32_t sx(sgn(dx));  
   const int32_t sy(sgn(dy));  
-  const int32_t ax(std::abs(dx)); 
-  const int32_t ay(std::abs(dy));  
+  const int32_t ax(abs((int32_t)dx)); 
+  const int32_t ay(abs((int32_t)dy));  
   const int32_t bx(2*ax);	
   const int32_t by(2*ay);	
   int32_t exy(ay-ax); // difference between x and y distances
